22a5feb37eb1f2bda9f064f1cb54eb2043727213
ampiatt <ampiatt@mail.umkc.edu>
2018-12-03 18:19:47 -0600

    updating DowJonesStocks

--- DowJonesStocks.py
+++ DowJonesStocks.py
@@ -2,6 +2,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.layers import Activation
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -16,13 +17,14 @@ def mydataset(dataset,back=1):
         Y.append(dataset[i+back,0]) #target
     return np.array(X),np.array(Y) # returns array of features and targets, here features are past prices and target is future price
 np.random.seed(7)
-df=pd.read_csv('yahoo2.csv',usecols=[4]) # getting data from the file, we are interested in the 4th column; the closing stocks
+df=pd.read_csv('DJIA_table.csv',usecols=[4]) # getting data from the file, we are interested in the 4th column; the closing stocks
 dataset=df.values
 dataset=dataset.astype('float32') # we convert our data into float type
 scaler=MinMaxScaler(feature_range=(0,1))
 dataset=scaler.fit_transform(dataset) # Randomly transform our dataset
 train_size=int(len(dataset)* 0.80) # getting 80 percent of our data as the training set
 test_size=len(dataset)-train_size  # test_size is the dataSize-trainingSize
+print(test_size)
 train,test=dataset[0:train_size,:],dataset[train_size:len(dataset),:]
 back=3 # shift our stock prices 3 steps backwards ,so that we can make future forcast
 # split the data into test and training set
@@ -33,12 +35,13 @@ X_train=np.reshape(X_train,(X_train.shape[0],1,X_train.shape[1]))
 X_test=np.reshape(X_test,(X_test.shape[0],1,X_test.shape[1]))
 # LSTM MODEL
 model=Sequential() # create an instance of sequential model
-model.add(LSTM(10,input_shape=(1,back))) # 1D LSTM model with 10 neurons
+model.add(LSTM(256,input_shape=(1,back))) # 1D LSTM model with 10 neurons
 model.add(Dropout(0.2)) # dropping out 20 % of my features
 model.add(Dense(1))
+model.add(Activation('sigmoid'))
 # Compiling , fitting and finding the accuracy of the model
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-history=model.fit(X_train,Y_train,epochs=10,batch_size=1,verbose=2)
+history=model.fit(X_train,Y_train,epochs=15,batch_size=30,verbose=2)
 scores = model.evaluate(X_test, Y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100)) # accuracy =scores[1], loss=scores[0]
 # Plotting the loss and accuracy graphs
--- stocks.py
+++ stocks.py
@@ -10,32 +10,32 @@ def readFile(filename):
         csvfileReader=csv.reader(csvfile)
         next(csvfileReader) # making sure all contents of file are read sequentially
         for r in csvfileReader:
-            date.append(int(r[0].split('/')[0]))# appending the dates value to a dates list, splitting date form mm/dd/yy
+            date.append(int(r[0].split('-')[0]))# appending the dates value to a dates list, splitting date form mm/dd/yy
             price.append(float(r[4])) # Closing stock price is in index 4 in the csv file, append it to price list
     return
 # fuction to predict prices based on a certain input date, implements Support Vector Machines
 # and return possible closing stock prices based on the SVM model
 def prediction(dates,prices,x):
     dates=np.reshape(dates,(len(dates),1)) # Reshapind dates array into a 1D array
-    lin=SVR(kernel='linear',C=1e3) # implements the linear model
+    # lin=SVR(kernel='linear',C=1e3) # implements the linear model
     polyy=SVR(kernel='poly',C=1e3,degree=2) # Polynomial model with deg 2
-    rBf=SVR(kernel='rbf',C=1e3,gamma=0.1) # implements the rbf model
+    # //rBf=SVR(kernel='rbf',C=1e3,gamma=0.1) # implements the rbf model
 
      # we will compare the results and see which of the above models gives a best bet
     # Fitting the dates and prices into the model
-    lin.fit(dates,prices)
+    # lin.fit(dates,prices)
     polyy.fit(dates, prices)
-    rBf.fit(dates, prices)
+    # rBf.fit(dates, prices)
     # Plotting the models
     plt.scatter(dates,prices,color='black',label='Data')
-    plt.plot(dates,rBf.predict(dates),color='red',label='RBF')
-    plt.plot(dates, lin.predict(dates), color='blue', label='Linear')
+    # plt.plot(dates,rBf.predict(dates),color='red',label='RBF')
+    # plt.plot(dates, lin.predict(dates), color='blue', label='Linear')
     plt.plot(dates, polyy.predict(dates), color='green', label='Polynomial')
     plt.xlabel('dates')
     plt.ylabel('prices')
     plt.legend()
     plt.show()
-    return polyy.predict(x)[0],lin.predict(x)[0],rBf.predict(x)[0]
-readFile('yahoo2.csv')
+    return polyy.predict(x)[0]
+readFile('DJIA_table.csv')
 predict=prediction(date,price,29)
 print(predict)
